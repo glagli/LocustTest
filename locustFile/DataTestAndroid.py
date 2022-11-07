871528e62123c16113ba811a806d0406dfd76792
Vladislav Glagolev <VIGlagolev@sberbank.ru>
2022-11-07 17:23:58 +0300

    Initial commit

---
+++ locustFile/DataTestAndroid.py
@@ -0,0 +1,243 @@
+# import random
+# import datetime
+# import hashlib
+#
+# todayManual = ["2022-10-01", "2022-10-02", "2022-10-03", "2022-10-04", "2022-10-05", "2022-10-06", "2022-10-07",
+#                "2022-10-08", "2022-10-09", "2022-10-10", "2022-10-11", "2022-10-12", "2022-10-13", "2022-10-14",
+#                "2022-10-15", "2022-10-16", "2022-10-17", "2022-10-18", "2022-10-19", "2022-10-20", "2022-10-21",
+#                "2022-10-22", "2022-10-23", "2022-10-24", "2022-10-25", "2022-10-26", "2022-10-27", "2022-10-28",
+#                "2022-10-29", "2022-10-30", "2022-10-31"]
+#
+# deviceAbi = ["armeabi-v7a", "arm64-v8a", "x86", "x86_64"]
+#
+# systemLanguage = ["en_GB", "ru_RU", "en_US", "en_CA", "en_RU", "de_DE", "ru_KZ", "en_VG", "ru_GB", "ru_UA", "ru_GE",
+#                   "ru_AS", "ru_US", "ru_FR", "en_AQ", "en_KZ", "ru_RO", "ru", "ru_NL", "ru_SB", "ru_IE", "ru_SC",
+#                   "ru_NO", "ru_TR", "en_ES", "ru_ES", "es_RU", "ru_JP", "en_BY", "ru_FI", "en", "ru_DE", "en_CH",
+#                   "de_US", "ru_BY", "ru_RS", "en_NL", "ru_SI", "en_IL", "en_DE", "ru_AM", "ru_EE", "ru_TH"]
+#
+# screenSize = ["2257x1080", "1080x2200", "2208x1080", "2264x1080", "2173x1080", "1920x1080", "2134x1080", "2030x1080",
+#               "2166x1080", "2287x1080", "1792x1080", "2200x1080", "2984x1440", "2110x1080", "2220x1080", "2129x1080",
+#               "2190x1080", "2178x1080", "2099x1080", "2094x1080", "2402x1080", "3120x1440", "2186x1080", "2034x1080",
+#               "2135x1080", "2263x1080", "2189x1080", "2278x1080", "2131x1080", "2265x1080", "2179x1080", "2125x1080",
+#               "2176x1080", "2210x1080", "1465x720", "1352x720", "2132x1080", "2085x1080", "2116x1080", "2143x1080",
+#               "2211x1080", "1382x720", "2180x1080", "2184x1080", "2164x1080", "2020x1080", "1396x720", "2368x1440",
+#               "1922x1536", "2137x1080", "1344x720", "1794x1080", "2060x1080", "2076x1080", "2183x1080", "2214x1080",
+#               "2260x1080", "1507x720", "2273x1080", "1184x720", "2123x1080", "2560x1440", "2361x1080", "2172x1080",
+#               "2177x1080", "2147x1080", "2860x1440", "2904x1440", "2064x1080", "1449x720", "2768x1440", "2192x1080",
+#               "2028x1080", "2043x1080", "2499x1200", "2341x1080", "2139x1080", "2088x1080", "1450x720", "2181x1080",
+#               "1384x720", "3060x1440", "2267x1080", "2252x1080", "2225x1080", "2082x1768", "1411x720", "2029x1080",
+#               "2128x1080", "2412x1440", "2206x1080", "2158x1080", "2130x1080", "1782x1080", "1423x720", "2115x1080",
+#               "2302x1080", "2246x1080", "2296x1080", "2198x1080"]
+#
+# apiKey = ["4faa2e69c853f26b144e0eaa06a6b5793df461bf439b68cfe856289510f850ef",
+#           "c61d4c65a6bbd7cc2f012c30c683511170a6e8ca04648af612476e4b3fc21ab6",
+#           "38ab27095c197a3960dda31a837595eb5f3149f4fba55081033ba14b411c2f27"]
+#
+# operationSystemVersion = ["29", "30", "31", "24", "28", "26", "32", "25", "33", "27", "23", "22", "21"]
+#
+# deviceVendor = ["vsmart", "TCL", "TECNO", "ZUK", "motorola", "HTC", "LGE", "nubia", "OUKITEL", "p12", "vivo", "Google",
+#                 "AYYA", "OnePlus", "samsung", "Xiaomi", "meizu", "OPPO", "unknown", "ZTE", "Blackview", "incar",
+#                 "HONOR", "asus", "Yandex", "BlackBerry", "HUAWEI", "alps", "Sony", "HMD Global", "realme", "Meizu",
+#                 "SHARP", "Highscreen", "LENOVO", "BullittGroupLimited", "BQru", "Apple", "sprd", "bq", "Hisense",
+#                 "TECNO MOBILE LIMITED", "Ulefone", "DOOGEE", "Wileyfox", "Huawei", "Lenovo", "vernee"]
+#
+# browser = ["Android"]
+#
+# deviceModel = ["Mi Note 10 Lite", "STK-LX1", "T775H", "RMX3151", "SM-A920F", "TECNO KH7n", "Pixel 3a", "H4311",
+#                "SM-M115F", "SM-A225F", "RMX3201", "SM-A530F", "RMX1931", "AOSP on IA Emulator", "vivo 1902", "iPad12,2",
+#                "SM-S901E", "Pixel 3a XL", "2107113SG", "SM-M225FV", "Z2 Plus", "Redmi 7", "SM-A305FN",
+#                "motorola one action", "KB2005", "SM-N975F", "HTC 10", "GLK-AL00", "E6683", "VTR-L29", "Nokia G50",
+#                "sdk_gphone_arm64", "SM-A307FN", "EML-L29", "PRO 7 Plus", "Mi A2 Lite", "SM-G925F", "Redmi 4A",
+#                "DUA-L22", "meizu note8", "2201117TY", "V2109", "M2103K19PY", "ONEPLUS A3010", "Nokia 5.1 Plus",
+#                "Pixel 3 XL", "AMZ-L29", "Mi MIX 2S", "SM-P615", "21091116UG", "SM-M317F", "LYA-L29", "SM-S908B",
+#                "SM-N9700", "SM-A750FN", "XT1585", "SM-G973N", "Pixel 3", "Nexus 5X", "Android SDK built for arm64",
+#                "ELS-NX9", "NX659J", "EBG-AN10", "ART-L29", "SM-A415F", "ANE-LX1", "SM-F916B", "RMX3370", "SM-N985F",
+#                "SM-F900F", "DUA-LX9", "Pixel 4", "2201116TG", "SM-G780F", "SM-S908E", "SM-A605FN", "MT2111", "MIX 2",
+#                "ELS-N39", "BE2029", "p12", "Redmi Note 7 Pro", "Mi A3", "PRO 7", "M2101K7BNY", "Nokia 7.2", "SNE-LX1",
+#                "DLI-TL20", "V2023", "SM-N980F", "SM-G935F", "IN2023", "SM-G960F", "ONEPLUS A6013", "Mi9 Pro 5G",
+#                "Mi 9 Lite"]
+#
+# operationSystem = ["Android"]
+#
+# deviceMemorySize = ["127948574720", "31993663488", "55941402624", "21003628544", "17179869184", "27229675520",
+#                     "10470940672", "24962768896", "11340582912", "11953950720", "10814251008", "255993663488",
+#                     "11887214592", "2570600448", "255989493760", "31993626624", "11983028224", "25713516544",
+#                     "11235057664", "27175149568", "57229148160", "499963174912", "4160421888", "15989440512",
+#                     "15989469184", "9837408256", "11275575296", "255989469184", "50256965632", "55376814080",
+#                     "63983177728", "63993663488", "15978983424", "3087351808", "8125857792", "127927603200",
+#                     "26266628096", "11454181376", "55937212416", "25486651392", "57107529728", "55292911616",
+#                     "121343569920"]
+#
+# platform = ["MOBILE"]
+# time = datetime.datetime.today().strftime("%H:%M:%S")
+#
+# body = {
+#     "meta": {
+#         "systemLanguage": random.choice(systemLanguage),
+#         "timeStamp": random.choice(todayManual) + "T" + time + ".668+03:00",
+#         "screenSize": random.choice(screenSize),
+#         "apiKey": random.choice(apiKey),
+#         "operationSystemVersion": random.choice(operationSystemVersion),
+#         "deviceVendor": random.choice(deviceVendor),
+#         "browser": browser,
+#         "deviceModel": random.choice(deviceModel),
+#         "operationSystem": random.choice(operationSystem),
+#         "deviceMemorySize": random.choice(deviceMemorySize),
+#         "platform": random.choice(platform),
+#         "deviceAbi": random.choice(deviceAbi)
+#     },
+#     "profile": {
+#         "appVersion": "12.2.0.3004_RC",
+#         "applicationLanguage": "de",
+#         "hashEpkId": "UNKNOWN",
+#         "appVersionNumber": "2022091511",
+#         "clientBlock": "ift-node7",
+#         "sessionId": random.choice(todayManual) + "T" + time + ".144+03:00 MswrBTYLvtH1xaTmxVxSNwxXIMAEBIoa",
+#         "hashUserLoginId": "uAQNRHhrhvI6frGUPJSKGgmNHuTrVx4f876PvSJZ5HGCVM6CDTQde9fTK2m1sMHU",
+#         "deviceId": "MswrBTYLvtH1xaTmxVxSNwxXIMAEBIoa",
+#         "hashEfsId": "UNKNOWN"
+#     },
+#     "data": [
+#         {
+#             "batteryLevel": "89",
+#             "connectionType": "CELLULAR",
+#             "eventType": "business",
+#             "geoLatitude": "55.7408994",
+#             "geoLongitude": "37.5318225",
+#             "internalIP": "10.8.7.35",
+#             "properties": [
+#                 {
+#                     "key": "internal_source",
+#                     "value": "Load"
+#                 },
+#                 {
+#                     "key": "DeliveryProductType",
+#                     "value": "94.9"
+#                 }
+#             ],
+#             "timeStamp": random.choice(todayManual) + "T" + time + ".417+03:00",
+#             "eventAction": "Main Show"
+#         }
+#     ]
+# }
+#
+# appVersion = ["14.1.0.7156", "14.3.0.3732_RC", "14.4.0.7770", "14.3.0.3736_RC", "14.0.0.2876_RC", "14.3.0.632",
+#               "14.4.0.7768",
+#               "14.4.0.7781", "12.18.0.2209_RC", "14.3.0.3733_RC", "14.4.0.7780", "12.20.0.6629", "14.3.0.7420",
+#               "14.3.0.3730_RC", "14.4.0.7779", "12.18.0.6037", "14.4.0.7778", "14.1.0.3162_RC", "12.15.0.1318_RC",
+#               "14.4.0.7769", "14.4.0.7771", "14.3.0.3731_RC", "14.4.0.7774", "12.15.0.5149", "12.13.0.4535",
+#               "14.4.0.7703",
+#               "12.17.0.1863_RC", "14.3.0.3727_RC", "14.3.0.3723_RC", "14.3.0.3721_RC", "12.16.0.5438", "14.3.0.3712_RC",
+#               "12.17.0.5680", "14.1.0.3164_RC", "14.0.0.6882", "14.2.0.3563_RC", "12.17.0.5613", "14.3.0.3720_RC",
+#               "14.4.0.7767", "14.2.0.7372", "14.2.0.3417_RC", "12.19.0.6337", "14.4.0.7760", "12.13.0.4738",
+#               "14.4.0.7766",
+#               "12.14.0.4925", "12.2.0.3004_RC", "12.18.0.5932", "14.4.0.7759", "12.4.0.170", "14.4.0.7765",
+#               "14.3.0.3710_RC",
+#               "14.4.0.7756", "14.3.0.626_RC", "12.16.0.1559_RC", "14.3.0.7454", "14.3.0.3675_RC", "14.4.0.7755",
+#               "14.0.0.2885_RC", "14.3.0.3708_RC", "12.20.0.2647_RC", "12.17.0.5663", "12.17.0.5750", "14.3.0.3707_RC",
+#               "12.19.0.6366", "14.4.0.7754", "12.16.0.1606_RC", "12.17.0.5626", "12.14.0.1049_RC", "12.16.0.5512",
+#               "12.21.0.6704", "12.19.0.2393_RC", "12.17.0.5652", "14.4.0.7753", "14.4.0.7720", "14.4.0.7734",
+#               "14.4.0.7723",
+#               "12.15.0.1269_RC", "12.4.0.39_RC", "12.5.0.3528", "12.19.0.2486_RC", "12.20.0.6656", "14.3.0.3674_RC",
+#               "14.4.0.631", "14.4.0.7730", "12.16.0.5329", "12.19.0.0", "14.3.0.3673_RC", "12.20.0.2813_RC",
+#               "14.3.0.3672_RC", "14.4.0.7718", "14.4.0.7719", "12.17.0.5704", "14.3.0.3671_RC", "12.16.0.5534",
+#               "14.3.0.3660_RC", "14.1.0.3132_RC", "14.4.0.7710", "14.4.0.7717", "12.20.0.2706_RC"]
+#
+# applicationLanguage = ["de", "en", "ru", "bg", "iw"]
+#
+# appVersionNumber = ["2022091608", "2022062815", "2022091514", "2022090822", "2022040517", "2022091516", "2022041111",
+#                     "2022091517", "2022091512", "2022090718", "2022091515", "2022080918", "2022090618", "2022090820",
+#                     "2022051315", "2022062713", "2022051713", "2022091510", "2022091511", "2022081013", "2022091508",
+#                     "2022091408", "2022091305", "2022072007", "2022091410", "2022062915", "2022091416", "2022091505",
+#                     "2022091504", "2022053015", "2022020313", "2022040810", "2022080414", "2022091308", "2022091415",
+#                     "2022061606", "2022091412", "2022030413", "2022071815", "2022031613", "2022040812", "2022021121",
+#                     "2022091413", "2022051214", "2022070610", "2022041310", "2022091411", "2022031713", "2021092210",
+#                     "2022091319", "2022091318", "2022060810", "2022091405", "2022082411", "2022091317", "2022040708",
+#                     "2022091323", "2022040616", "2022031519", "2022090920", "2022040723", "2022053008", "2022061612",
+#                     "2022041915", "2021070614", "2022011206", "2022091315", "2022022017", "2022022108", "2022091217",
+#                     "2022091312", "2022052712", "2022012713", "2022080818", "2022091313", "2021100512", "2022022112",
+#                     "2022091122", "2022091216", "2022091310", "2022051817", "2022040608", "2021101108", "2022091311",
+#                     "2021092411", "2022081113", "2021102220", "2022070506", "2021080314", "2022091306", "2022041110",
+#                     "2022081815", "2022053108", "2022090814", "2022090518", "2022072910", "2022070710", "2022090808",
+#                     "2022020209", "2022022505"]
+#
+# clientBlock = ["node5", "mobile-greenfield1", "node4", "mobile-psinode1", "ift-node10", "node0", "ift-node1", "node7",
+#                "ift-node9", "ift-node0", "node3", "ift-node5", "ift-node7", "mobile-node1", "newTestNodes01092022_2",
+#                "greenfield1", "node6.online.sberbank.ru", "nginx-and-ift-b5", "nginx-and-ift-b1",
+#                "mobile-psinode6.testonline.sberbank.ru", "nginx-and-ift-b7", "nginx-and-ift-si1",
+#                "ift-node1-mp.testonline.sberbank.ru", "ift-node10-mp", "ift-node10-mp.testonline.sberbank.ru",
+#                "ift-node5-mp.testonline.sberbank.ru", "mobile-psi-node1", "nginx-and-ift-si3", "psinode6",
+#                "mobile-node2", "mobile-psi-nodesi1", "mobile-node4", "mobile-psinode1.testonline.sberbank.ru",
+#                "node2", "mobile-psinode4", "psinode2", "ift-node2", "mobile-psi-nodesi2",
+#                "nginx-ios-ift-b3.testonline.sberbank.ru", "mobile-psinode2.testonline.sberbank.ru",
+#                "nginx-ios-ift-b1.testonline.sberbank.ru", "node3.online.sberbank.ru",
+#                "ift-node7-mp.testonline.sberbank.ru", "mobile-psi-node2", "node1", "ift-node5-mp", "ift-node9-mp",
+#                "ift-node2-mp", "mobile-node3", "ift-node0-mp", "psinode4", "ift-node2-mp.testonline.sberbank.ru",
+#                "mobile-psinode4.testonline.sberbank.ru", "psinode1", "nginx-and-ift-b3", "ift-node7-mp",
+#                "194.186.207.23",
+#                "greenfield1.online.sberbank.ru", "ift-node0-mp.testonline.sberbank.ru",
+#                "ift-node9-mp.testonline.sberbank.ru", "ift-guest.testonline.sberbank.ru", "ift-guest", "ift-node1-mp",
+#                "ift-node4", "mobile-node5", "nginx-ios-ift-si1.testonline.sberbank.ru", "psinode7", "mobile-node7",
+#                "psinode6.testonline.sberbank.ru", "node7.online.sberbank.ru", "ift-sowa-node11", "online",
+#                "https://devnode2", "mobile-node6", "ift-node8.testonline.sberbank.ru", "ift-node8",
+#                "node5.online.sberbank.ru", "ift-sowa-node11.testonline.sberbank.ru",
+#                "ift-node4-mp.testonline.sberbank.ru", "node4.online.sberbank.ru", "node0.online.sberbank.ru",
+#                "nginx-ios-ift-b3", "nginx-ios-ift-b5", "nginx-ios-ift-b5.testonline.sberbank.ru",
+#                "node2.online.sberbank.ru", "node1.online.sberbank.ru", "mobile-psinode2", "ift-node12-mp",
+#                "ift-node12-mp.testonline.sberbank.ru", "mobile-psinode6", "mobile-psinode5.testonline.sberbank.ru",
+#                "greenfield1.online.sberbank.ru:4477", "ufsuko-and-ift-b7", "194", "node6", "mobile-psinode5",
+#                "tkli-efs2148", "mapi", "ift-node4-mp"]
+#
+#
+# LoginId_deviceId = {
+#     hashlib.sha256(str(random.randrange(1000)).encode('ascii')).hexdigest(): [
+#         hashlib.md5(str(random.randrange(10000)).encode('ascii')).hexdigest(),
+#         hashlib.md5(str(random.randrange(10000)).encode('ascii')).hexdigest(),
+#         hashlib.md5(str(random.randrange(10000)).encode('ascii')).hexdigest()
+#     ]
+# }
+#
+# login_hash = list(LoginId_deviceId.keys())[0]
+# devices_hash = random.choice(list(LoginId_deviceId.values())[0])
+#
+#
+#
+#
+# #
+# # #
+# # // Блок DATA
+# #
+# # batteryLevel = String(_.random(1, 100)),
+# # pm.collectionVariables.set("batteryLevel", batteryLevel);
+# #
+# # connectionType = ["UNKNOWN","CELLULAR","WIFI"];
+# # pm.collectionVariables.set("connectionType", connectionType[_.random(0, connectionType.length-1)]);
+# #
+# #
+# # eventType = ["business","technical","business","business"];
+# # pm.collectionVariables.set("eventType", eventType[_.random(0, eventType.length-1)]);
+# #
+# #
+# # key = ["contractTag","ABflag","EXCurr","Gate","external_source","internal_source","CardCount","DetailsFrom","From","ChangedEmbossingName","DeliveryProductType","EntryPoint","PlasticChosen","ProductTypeMetric","ReissueReason","biometry","compositeProducts","contactsSync","controlConsentUnify","csa","extendedPermission","firebaseRemoteConfig","googlePay","greetingUpdate","launcher","loanCardOffer","rates","samsungPay","sberKids","sberPay","sendingAdvertisingId","session","settings","updatePushToken","AnalyticalAccount","DetailsFrom","GroupCode","ProviderName","ServiceID","ServiceName"];
+# # pm.collectionVariables.set("key", key[_.random(0, key.length-1)]);
+# # pm.collectionVariables.set("key2", key[_.random(0, key.length-1)]);
+# # pm.collectionVariables.set("key3", key[_.random(0, key.length-1)]);
+# #
+# #
+# # value = ["organic","mobile-psi-node2.testonline.sberbank.ru","200","organic","94.9","true","3.8","cbro-t","true","false","udp","Network", "Load"];
+# # pm.collectionVariables.set("value", value[_.random(0, value.length-1)]);
+# # pm.collectionVariables.set("value2", value[_.random(0, value.length-1)]);
+# # pm.collectionVariables.set("value3", value[_.random(0, value.length-1)]);
+# #
+# # eventAction = ["Card GeneralScreen Click","Marketplace Menu Item Show","Auth Animation Show","Sales Tools Offer Tap","Main Show","Auth Welcome Show","Auth timer","Sales Tools Offer Show","Investments InvestAccClosure TechnicalError","Assign Landing Click"];
+# # eventAction2 = ["PFM Category Deeplink Denied","BICYCLE assistant_get_ufsInfo","MainNBAA widgets downloaded","Main Section Displayed","assistant_tabbar_control_show","BICYCLE Audio Packet Lag","Sales Tools Story Preview Show","Main Slider Widgets Displayed","Sales Tools Offer Error Respond","assistant_remote_res_mapping_started","App Permissions","assistant_response_received","Dialogs Dialogs Tab is disabled","ClientProfile Task Show","Push On New Push","Initial data strategy universal","InitialDataLoader Load","assistant_remote_res_mapping_success","DGis SDK Initialization Finished","Sales Tools Offer Server Respond"];
+# # pm.collectionVariables.set("eventAction", eventAction[_.random(0, eventAction.length-1)]);
+# # pm.collectionVariables.set("eventAction2", eventAction2[_.random(0, eventAction2.length-1)]);
+# # pm.collectionVariables.set("eventAction3", eventAction2[_.random(0, eventAction2.length-1)]);
+# # pm.collectionVariables.set("eventAction4", eventAction[_.random(0, eventAction.length-1)]);
+# # pm.collectionVariables.set("eventAction5", eventAction2[_.random(0, eventAction2.length-1)]);
+# # pm.collectionVariables.set("eventAction6", eventAction2[_.random(0, eventAction2.length-1)]);
+# # pm.collectionVariables.set("eventAction7", eventAction[_.random(0, eventAction.length-1)]);
+# # pm.collectionVariables.set("eventAction8", eventAction2[_.random(0, eventAction2.length-1)]);
+# # pm.collectionVariables.set("eventAction9", eventAction2[_.random(0, eventAction2.length-1)]);
+# # pm.collectionVariables.set("eventAction10", eventAction[_.random(0, eventAction.length-1)]);
